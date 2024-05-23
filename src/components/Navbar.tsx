bebd0f73f9e9f5d9f4d174e0084ee09592673c45
Steeve-Johan Otoka-Eyota <73400373+johanotoka@users.noreply.github.com>
2024-05-23 03:35:15 -0400

    Update Navbar.tsx
    fixed h2 being a child of p

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,10 +10,10 @@ export default function Navbar({}: Props) {
   return (
     <div className='shadow-sm sticky top-0 left-0 z-50 bg-white'>
         <div className='h-[80px] w-full flex justify-between items-center max-w-7xl px-3 mx-auto'>
-            <p className='flex items-center justify-center gap-2'>
+            <div className='flex items-center justify-center gap-2'>
                 <h2 className='text-gray-500 text-3xl'>Weatheria</h2>
                 <PiSunHorizonFill className='text-3xl mt-1 text-orange-300'/>
-            </p>
+            </div>
             {/*  */}
             <section className='flex gap-2 items-center'>
                 <MdMyLocation className='text-3xl text-gray-400 hover:opacity-80 cursor-pointer'/>
